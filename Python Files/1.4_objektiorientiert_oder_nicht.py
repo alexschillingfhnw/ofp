6242afc3180a44938b7a75258765f6526786badb
alexschillingfhnw <90834423+alexschillingfhnw@users.noreply.github.com>
2023-06-12 09:01:35 +0200

    update objektorientiert oder nicht file

--- Python Files/1.4_objektiorientiert_oder_nicht.py
+++ Python Files/1.4_objektiorientiert_oder_nicht.py
@@ -15,3 +15,15 @@ def new_averager() -> Averager:
         return state[tot] / state[num]
 
     return add_value, get_average
+
+
+# Erstellt eine neue Instanz von Averager
+add_value, get_average = new_averager()
+
+# Fügt einige Werte hinzu
+add_value(10)
+add_value(20)
+add_value(30)
+
+# Gibt den aktuellen Durchschnitt aus
+print("Durchschnitt: ", get_average())
